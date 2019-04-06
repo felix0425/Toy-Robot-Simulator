323ff27d81f5025f47d77274e5358c3eae170abc
felix0425 <felix960425@gmail.com>
2019-04-06 22:20:47 +0100

    Add files via upload

---
+++ Toy/UnitTesting/UnitTest1.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Program;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestInitialize]
+        public void testInit()
+        {
+            Program p = new Program();
+            Robot myRobot = new Robot();
+            myRobot.setBoard(0, 0, 4, 4);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
